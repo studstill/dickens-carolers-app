7b6a2330f7ceae355cc615c077a9c4cb82044ae4
Jason Studstill <jason@jasonstudstill.com>
2015-11-08 12:48:11 -0800

    Abandon CSS card reveal in favor of react logic

--- app/js/components/flash-cards.jsx
+++ app/js/components/flash-cards.jsx
@@ -5,12 +5,12 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {
       songList: {},
-      currentSongTitle: '',
+      currentSongTitle: 'Click "New Song" for a random song',
       currentSongKey: '',
       currentSongNumber: 0,
       keyRevealed: false,
       songPicked: false,
-      revealed: true
+      revealed: false
     };
   },
 
@@ -28,13 +28,6 @@ module.exports = React.createClass({
   },
 
   rotateCard: function() {
-    console.log(this.state);
-    var f1_card = document.getElementById('f1_card');
-    if (this.state.revealed) {
-      f1_card.classList.add('revealed');
-    } else {
-      f1_card.classList.remove('revealed');
-    }
     this.setState({
       revealed: !this.state.revealed
     });
@@ -46,13 +39,19 @@ module.exports = React.createClass({
         <div className="flashCards">
           <div id="f1_container">
             <div id="f1_card" className="card">
-              <div className="front face">
-                <p>{this.state.currentSongTitle}</p>
-              </div>
-              <div className="back face center">
-                <h2>Number: {this.state.currentSongNumber}</h2>
-                <h2>Key: {this.state.currentSongKey}</h2>
-              </div>
+              {this.state.revealed ?
+                (
+                  <div className="back face center">
+                    <h2>Number: {this.state.currentSongNumber}</h2>
+                    <h2>Key: {this.state.currentSongKey}</h2>
+                  </div>
+                ) :
+                (
+                  <div className="front face">
+                    <p>{this.state.currentSongTitle}</p>
+                  </div>
+                )
+              }
             </div>
           </div>
           <button onClick={this.pickRandomSong}>New Song</button>
